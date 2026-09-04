046e0622b6647ead8ad13d21ee1b0fe56486f9bf
originalRecipe1 <dev@ogrcp.de>
2026-09-04 13:36:07 +0200

    build: prepare experiment 2 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -124,8 +124,8 @@ android {
         applicationId = "org.peek.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0-experiment.1"
+        versionCode = 2
+        versionName = "0.1.0-experiment.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField(
