8c9b05791b94fa4c09f47d516f64b5e59e18eae5
originalRecipe1 <dev@ogrcp.de>
2026-09-04 09:41:32 +0200

    ci: provision managed Android test device

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -141,6 +141,7 @@ android {
                     device = "Pixel 2"
                     apiLevel = 30
                     systemImageSource = "aosp-atd"
+                    testedAbi = "x86"
                 }
             }
         }
